343f65948b95ed73fb584aded2d9eb5b5f1ac92c
Komil00 <komiltuev@gmail.com>
2022-04-05 01:03:35 +0500

    first commit

---
+++ news/views.py
@@ -0,0 +1,12 @@
+from pyexpat import model
+from django.shortcuts import render
+
+from .models import Post
+from django.views.generic import ListView
+
+# Create your views here.
+
+
+class HomePageView(ListView):
+    model = Post
+    template_name = 'home.html'
